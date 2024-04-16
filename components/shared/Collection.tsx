c7c0e3ae7b5ceb5973750f55aeb7dba6ce66ad10
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-04-16 22:35:37 +0530

    commit2

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,8 +1,7 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import React from 'react'
 import Card from './Card'
-
-
+import Pagination from './Pagination'
 
 type CollectionProps = {
   data: IEvent[],
@@ -26,9 +25,9 @@ const Collection = ({
 }: CollectionProps) => {
   return (
     <>
-      {data.length > 0? (
-        <div className='flex flex-col items-center gap-10'>
-            <ul className='grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10'>
+      {data.length > 0 ? (
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {data.map((event) => {
               const hasOrderLink = collectionType === 'Events_Organized';
               const hidePrice = collectionType === 'My_Tickets';
@@ -39,15 +38,20 @@ const Collection = ({
                 </li>
               )
             })}
-            </ul>
+          </ul>
+
+          {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
+          )}
         </div>
       ): (
-        <div className='flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center'>
-             <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
-             <p className="p-regular-14">{emptyStateSubtext}</p>
+        <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
         </div>
-      )}
+      )} 
     </>
   )
 }
+
 export default Collection
